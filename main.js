6415e82444c223a328a5943208a2e580a7686d02
Nicholas <nicholas.cruickshank25@gmail.com>
2024-07-29 18:09:55 +1000

    added some animation to the text

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 // ACTUAL SHIT
 
-const texts = document.querySelectorAll(".text");
-
 // took this and made work with vanilla js
 // https://dev.to/selbekk/how-to-fade-in-content-as-it-scrolls-into-view-10j4
 // MDN for Instersection API: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
@@ -11,13 +9,27 @@ const observer = new IntersectionObserver(
     entries.forEach((entry) => {
       const element = entry.target;
       if (entry.isIntersecting) {
-        element.classList.add("visible");
+        if (element.classList.contains("text")) {
+          return element.classList.add("visible");
+        }
+
+        if (element.classList.contains("love")) {
+          return element.classList.add("shadow-animation");
+        }
       } else {
-        element.classList.remove("visible");
+        if (element.classList.contains("text")) {
+          return element.classList.remove("visible");
+        }
+
+        if (element.classList.contains("love")) {
+          return element.classList.remove("shadow-animation");
+        }
       }
     });
   },
   { threshold: 0.4, rootMargin: "16px" }
 );
 
-texts.forEach((ele) => observer.observe(ele));
+// OBSERVERS
+document.querySelectorAll(".text").forEach((ele) => observer.observe(ele));
+observer.observe(document.querySelector(".love"));
